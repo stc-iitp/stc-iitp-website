f7e1e0a5c3481cb1984f1b929e63b75f668677e6
satushankar <krish4632stella@gmail.com>
2026-08-20 10:37:27 +0530

    feat(home): hide placeholder Current Events section
    The section rendered template filler (EVENT NAME 1-4, CLUB NAME,
    a repeated placeholder date and image). Commented out rather than
    deleted so it can be restored once real event data is available.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import HeroSection from "@/components/HeroSection";
 import MissionSection from "@/components/MissionSection";
-import EventsSection from "@/components/EventsSection";
+// import EventsSection from "@/components/EventsSection";
 import OneIITP from "@/components/OneIITP";
 import CollabNest from "@/components/CollabNest";
+import Alloc8 from "@/components/Alloc8";
 import Celesta from "@/components/Celesta";
 import AboutSection from "@/components/AboutSection";
 import PhotosSection from "@/components/PhotosSection";
@@ -17,9 +18,10 @@ export default function Home() {
         <HeroSection />
         <ScrollReveal><div className="px-4 md:px-16"><AboutSection /></div></ScrollReveal>
         <ScrollReveal><MissionSection /></ScrollReveal>
-        <ScrollReveal><EventsSection /></ScrollReveal>
+        {/* <ScrollReveal><EventsSection /></ScrollReveal> */}
         <ScrollReveal><OneIITP /></ScrollReveal>
         <ScrollReveal><CollabNest /></ScrollReveal>
+        <ScrollReveal><Alloc8 /></ScrollReveal>
         <ScrollReveal><StudentClubsSection /></ScrollReveal>
         <ScrollReveal><Celesta /></ScrollReveal>
         <ScrollReveal><PhotosSection /></ScrollReveal>
